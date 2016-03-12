04fc7a861a1d0ddb453c53aa894a0931a968d46a
naftalmm <naftalmm@gmail.com>
2016-03-12 20:01:31 +0300

    add stop command

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ TOKEN = os.environ['TOKEN']
 reply_awaiting_function = None
 all_banks = checker.get_bank_names()
 
+
 def log_params(method_name, update):
     logger.debug("Method: %s\nFrom: %s\nchat_id: %d\nText: %s" %
                  (method_name,
@@ -27,7 +28,8 @@ def help(bot, update):
 /help - Помощь
 /addbank - Добавить банк в список интересующих
 /removebank - Удалить банк из списка интересующих
-/getbanksstatuses - Получить информацию о состоянии банков из списка""", reply_markup=ReplyKeyboardHide())
+/getbanksstatuses - Получить информацию о состоянии банков из списка
+/stop - Прекратить получать уведомления, очистить список""", reply_markup=ReplyKeyboardHide())
 
 
 def add_bank(bot, update, args):
@@ -54,9 +56,9 @@ def add_bank(bot, update, args):
                             reply_markup=(get_choose_bank_keyboard(bank_names_guesses_list)))
 
 
-def add_subscription(telegram_user, bank_name):
+def add_subscription(telegram_user_id, bank_name):
     if bank_name in all_banks:
-        return checker.add_subscription(telegram_user.id, checker.get_bank_id(bank_name))
+        return checker.add_subscription(telegram_user_id, checker.get_bank_id(bank_name))
     else:
         return "Никогда не слышал о таком банке"
 
@@ -93,22 +95,28 @@ def remove_bank(bot, update):
                         reply_markup=(get_choose_bank_keyboard(user_banks_names)))
 
 
-def remove_subscription(telegram_user, bank_name):
+def remove_subscription(telegram_user_id, bank_name):
     if bank_name in all_banks:
-        return checker.remove_subscription(telegram_user.id, checker.get_bank_id(bank_name))
+        return checker.remove_subscription(telegram_user_id, checker.get_bank_id(bank_name))
     else:
         return "Никогда не слышал о таком банке"
 
 
+def stop(bot, update):
+    checker.remove_user(update.message.from_user.id)
+    bot.sendMessage(update.message.chat_id,
+                    text="Вы всегда можете начать общение со мной заново по команде /start",
+                    reply_markup=ReplyKeyboardHide())
+
 bot_functions = {"help": help, "addbank": add_bank, "removebank": remove_bank,
-                 "getbanksstatuses": get_banks_statuses}
+                 "getbanksstatuses": get_banks_statuses, "stop": stop}
 
 
 def bank_name_answer_handler(bot, update, args):
     global reply_awaiting_function
     if reply_awaiting_function is not None:
         # noinspection PyCallingNonCallable
-        reply = reply_awaiting_function(update.message.from_user, update.message.text)
+        reply = reply_awaiting_function(update.message.from_user.id, update.message.text)
         bot.sendMessage(update.message.chat_id,
                         text=reply,
                         reply_markup=ReplyKeyboardHide())
