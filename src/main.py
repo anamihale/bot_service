7fa828738efb4bea1fba77d09729b71ae1f609d4
naftalmm <naftalmm@gmail.com>
2016-03-12 18:49:10 +0300

    refactored

--- src/main.py
+++ src/main.py
@@ -10,36 +10,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 TOKEN = os.environ['TOKEN']
-
-users = dict() #TODO server-side
-
 reply_awaiting_function = None
-
-
-class UserInfo:
-    def __init__(self, telegram_user):
-        #TODO server-side
-        self.user = telegram_user
-        self.banks = dict()
-
-    def add_bank(self, bank_name):
-        #TODO server-side
-        self.banks[bank_name] = Bank(bank_name)
-
-    def remove_bank(self, bank_name):
-        #TODO server-side
-        self.banks.pop(bank_name)
-
-
-class Bank:
-    def __init__(self, bank_name):
-        self.bank_name = bank_name
-        self.bank_id = checker.get_bank_id_by_name(bank_name)
-
-    def get_status(self):
-        logger.info("Getting status for bank %s" % self.bank_id)
-        return checker.get_status(self.bank_id)
-
+all_banks = checker.get_all_banks_names()
 
 def log_params(method_name, update):
     logger.debug("Method: %s\nFrom: %s\nchat_id: %d\nText: %s" %
@@ -62,32 +34,31 @@ def add_bank(bot, update, args):
     global reply_awaiting_function
     log_params('add_bank', update)
     if len(args) == 0:
-        bot.sendMessage(update.message.chat_id, text="Использование:\n/addbank [Название]")
-        return
-    bank_name_guess = " ".join(args)
-    bank_names_guesses_list = checker.get_bank_name_guesses(bank_name_guess)  # key = id, value = name
-    if len(bank_names_guesses_list) == 0:
-        bot.sendMessage(update.message.chat_id, text="Никогда не слышал о таком банке")
-    elif len(bank_names_guesses_list) == 1:
-        telegram_user = update.message.from_user
-        bot.sendMessage(update.message.chat_id, text=add_bank_by_name(telegram_user, bank_names_guesses_list[0]))
+        reply_awaiting_function = add_subscription
+        bot.sendMessage(update.message.chat_id, text="Использование:\n/addbank [Название]\n"
+                                                     "Но можете попробовать найти свой банк в полном списке",
+                        reply_markup=(get_choose_bank_keyboard(sorted(all_banks))))
     else:
-        reply_awaiting_function = "add_bank"
-        bot.sendMessage(update.message.chat_id,
-                        text="Существует несколько банков с похожим названием. Какой вы имели в виду?",
-                        reply_markup=(ReplyKeyboardMarkup(get_choose_bank_keyboard(bank_names_guesses_list))))
+        bank_name_guess = " ".join(args)
+        bank_names_guesses_list = checker.get_bank_name_guesses(bank_name_guess)
+        if len(bank_names_guesses_list) == 0:
+            bot.sendMessage(update.message.chat_id, text="Никогда не слышал о таком банке")
+        elif len(bank_names_guesses_list) == 1:
+            telegram_user = update.message.from_user.id
+            reply = add_subscription(telegram_user, bank_names_guesses_list[0])
+            bot.sendMessage(update.message.chat_id, text=reply)
+        else:
+            reply_awaiting_function = add_subscription
+            bot.sendMessage(update.message.chat_id,
+                            text="Существует несколько банков с похожим названием. Какой вы имели в виду?",
+                            reply_markup=(get_choose_bank_keyboard(bank_names_guesses_list)))
 
 
-def add_bank_by_name(telegram_user, bank_name):
-    if telegram_user.id not in users.keys():
-        users[telegram_user.id] = UserInfo(telegram_user)
-
-    user = users[telegram_user.id]
-    if bank_name not in user.banks.keys():
-        user.add_bank(bank_name)
-        return "Банк успешно добавлен"
+def add_subscription(telegram_user, bank_name):
+    if bank_name in all_banks:
+        return checker.add_subscription(telegram_user, bank_name)
     else:
-        return "Банк уже есть в списке"
+        return "Никогда не слышал о таком банке"
 
 
 def get_choose_bank_keyboard(bank_names_list):
@@ -96,49 +67,37 @@ def get_choose_bank_keyboard(bank_names_list):
 
 
 def get_banks_statuses(bot, update, args):
-    telegram_user = update.message.from_user
-    if telegram_user.id not in users.keys():
-        users[telegram_user.id] = UserInfo(telegram_user)
-
-    user = users[telegram_user.id]
-    if len(user.banks) < 1:
+    telegram_user = update.message.from_user.id
+    user_banks_names = checker.get_user_subscriptions(telegram_user)
+    if len(user_banks_names) < 1:
         bot.sendMessage(update.message.chat_id,
                         text="Список банков пуст. Сперва добавьте банк в список с помощью команды\n"
                              "/addbank [Название]")
     else:
-        for bank in user.banks:
-            bot.sendMessage(update.message.chat_id, text=bank.get_status())
+        for bank in user_banks_names:
+            bot.sendMessage(update.message.chat_id, text=checker.get_status(bank))
 
 
 def remove_bank(bot, update):
     global reply_awaiting_function
-    telegram_user = update.message.from_user
-    if telegram_user.id not in users.keys():
-        users[telegram_user.id] = UserInfo(telegram_user)
-
-    user = users[telegram_user.id]
-    if len(user.banks) < 1:
+    telegram_user = update.message.from_user.id
+    user_banks_names = checker.get_user_subscriptions(telegram_user)
+    if len(user_banks_names) < 1:
         bot.sendMessage(update.message.chat_id,
                         text="Список банков пуст. Сперва добавьте банк в список с помощью команды\n"
                              "/addbank [Название]")
     else:
-        reply_awaiting_function = "remove_bank"
+        reply_awaiting_function = remove_subscription
         bot.sendMessage(update.message.chat_id,
                         text="Выберите банк",
-                        reply_markup=(get_choose_bank_keyboard(user.banks.keys())))
+                        reply_markup=(get_choose_bank_keyboard(user_banks_names)))
 
 
-def remove_bank_by_name(telegram_user, bank_name):
-    bank_id = checker.get_bank_name_guesses(bank_name)
-    if telegram_user.id not in users.keys():
-        users[telegram_user.id] = UserInfo(telegram_user)
-
-    user = users[telegram_user.id]
-    if bank_name not in user.banks.keys():
-        user.remove_bank(bank_id)
-        return "Банк успешно удален"
+def remove_subscription(telegram_user, bank_name):
+    if bank_name in all_banks:
+        return checker.remove_subscription(telegram_user, bank_name)
     else:
-        return "Банка не было в списке"
+        return "Никогда не слышал о таком банке"
 
 
 bot_functions = {"help": help, "addbank": add_bank, "removebank": remove_bank,
@@ -147,13 +106,11 @@ bot_functions = {"help": help, "addbank": add_bank, "removebank": remove_bank,
 
 def bank_name_answer_handler(bot, update, args):
     global reply_awaiting_function
-    if reply_awaiting_function == "remove_bank":
+    if reply_awaiting_function is not None:
+        # noinspection PyCallingNonCallable
+        reply = reply_awaiting_function(update.message.from_user, update.message.text)
         bot.sendMessage(update.message.chat_id,
-                        text=remove_bank_by_name(update.message.from_user, update.message.text),
-                        reply_markup=ReplyKeyboardHide())
-    elif reply_awaiting_function == "add_bank":
-        bot.sendMessage(update.message.chat_id,
-                        text=add_bank_by_name(update.message.from_user, update.message.text),
+                        text=reply,
                         reply_markup=ReplyKeyboardHide())
 
     reply_awaiting_function = None
