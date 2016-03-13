f5a9bacdcabf77a5551705a6dfbfdfc64b18de6d
naftalmm <naftalmm@gmail.com>
2016-03-13 11:24:46 +0300

    fix notification

--- src/notification.py
+++ src/notification.py
@@ -4,20 +4,18 @@ from telegram import Bot
 
 TOKEN = os.environ['TOKEN']
 
+subscriptions = checker.get_subscriptions()
+bot = Bot(TOKEN)
+users = set()
+print(subscriptions)
+for (user_id, bank_id) in subscriptions:
+    if user_id not in users:
+        users.add(user_id)
+        print("Оповещаю пользователя ", user_id)
+        bot.sendMessage(chat_id=int(user_id),
+                        text="Обновились данные о финансовом состоянии интересующих вас банков")
 
-def notify_users():
-    subscriptions = checker.get_subscriptions()
-    bot = Bot(TOKEN)
-    users = set()
-    print(subscriptions)
-    for (user_id, bank_id) in subscriptions:
-        if user_id not in users:
-            users.add(user_id)
-            print("Оповещаю пользователя ", user_id)
-            bot.sendMessage(chat_id=int(user_id),
-                            text="Обновились данные о финансовом состоянии интересующих вас банков")
-
-    for (user_id, bank_id) in subscriptions:
-        text = checker.get_status(checker.get_bank_name(bank_id))
-        print("Оповещаю пользователя ", user_id, " o том, что \n", text)
-        bot.sendMessage(chat_id=int(user_id), text=text)
+for (user_id, bank_id) in subscriptions:
+    text = checker.get_status(checker.get_bank_name(bank_id))
+    print("Оповещаю пользователя ", user_id, " o том, что \n", text)
+    bot.sendMessage(chat_id=int(user_id), text=text)
