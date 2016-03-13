5ba6bb013ee16760bea8471a994870828c1fc0b2
naftalmm <naftalmm@gmail.com>
2016-03-13 11:22:19 +0300

    try to fix notification add debug info

--- src/notification.py
+++ src/notification.py
@@ -9,10 +9,15 @@ def notify_users():
     subscriptions = checker.get_subscriptions()
     bot = Bot(TOKEN)
     users = set()
+    print(subscriptions)
     for (user_id, bank_id) in subscriptions:
         if user_id not in users:
             users.add(user_id)
-            bot.sendMessage(chat_id=user_id, text="Обновились данные о финансовом состоянии интересующих вас банков")
+            print("Оповещаю пользователя ", user_id)
+            bot.sendMessage(chat_id=int(user_id),
+                            text="Обновились данные о финансовом состоянии интересующих вас банков")
 
     for (user_id, bank_id) in subscriptions:
-        bot.sendMessage(chat_id=user_id, text=checker.get_status(checker.get_bank_name(bank_id)))
+        text = checker.get_status(checker.get_bank_name(bank_id))
+        print("Оповещаю пользователя ", user_id, " o том, что \n", text)
+        bot.sendMessage(chat_id=int(user_id), text=text)
